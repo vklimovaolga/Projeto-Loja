732eab5ee424b925554be5381847ec782915b55c
vklimovaolga <vklimova.olga@gmail.com>
2020-06-09 13:58:17 +0100

    Connection to DB

---
+++ Controllers/teste.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once("models/base.php");
+
+    $model = new Base();
+    // $testes = $model->teste();
+?>
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Home</title>
-    </head>
-    <body>
-    </body>
-</html>
+
+<?php 
+    $controllers = ["teste"];
+    $controller = $controllers[0];
+
+    define("ROOT", dirname($_SERVER["SCRIPT_NAME"]). "/");
+
+    $url_pastrs = explode("/", $_SERVER["REQUEST_URI"]);
+    
+    require("controllers/".$controller.".php");
+?>
+
+
+
